fb4c46f7823e6352a371ebd414c26c12eef116da
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-10 15:20:31 -0400

    Finished Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Classes
+﻿using System.Threading.Channels;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car newCar = new Car();
+            newCar.Make = "Corvette";
+            newCar.Model = "Stingray";
+            newCar.Year = 1970;
+
+            var toyota = new Car()
+            {
+               Make = "Toyota" ,
+               Model = "Corolla" ,
+               Year = 2016
+
+             };
+
+            var chevy = new Car("Chevy" , "Impala" , 2001);
+            var carList = new List<Car>() {newCar, toyota, chevy,};
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+
+
+
         }
+
     }
 }
